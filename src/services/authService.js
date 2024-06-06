7d534f62e879426dad824e89cd2a807cee62f9bb
Andres Quintero <quand.mind@gmail.com>
2024-06-06 11:01:30 -0400

    quit console

--- src/services/authService.js
+++ src/services/authService.js
@@ -31,7 +31,6 @@ const createJWT = (user) => {
         iat: moment().unix(),
         exp: moment().add(1, 'day').unix(),
     }
-    console.log(payload);
     return jwt.sign(payload, process.env.JWT_SECRET)
 }
 
