953aa05605c52840afb242403ad4b8fb61ef56bc
Andres Quintero <quand.mind@gmail.com>
2024-10-22 10:57:46 -0400

    asdasdasd

--- src/server.js
+++ src/server.js
@@ -110,7 +110,6 @@ app.listen(port, async () => {
           const mailOptions = {
             from: 'La Mundial de Seguros',
             // to: ['quand.mind@gmail.com'], // Cambia esto por la dirección de destino
-            // to: ['quand.mind@gmail.com'], // Cambia esto por la dirección de destino
             to: ['quand.mind@gmail.com', userResult.recordset[0].xemail], // Cambia esto por la dirección de destino
             subject: `Problemas en proceso de ${aviso.xnombre}`,
             html: emailHtml
